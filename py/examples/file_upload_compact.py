82ff438df534e2981f4ac79e0688219a70b36791
Martin Turoci <martin.turoci@h2o.ai>
2021-04-06 10:52:13 +0200

    chore: remove tmp testing code #623

--- py/examples/file_upload_compact.py
+++ py/examples/file_upload_compact.py
@@ -7,7 +7,6 @@ from h2o_wave import main, app, Q, ui
 
 @app('/demo')
 async def serve(q: Q):
-    print(q.args)
     if 'file_upload' in q.args:
         q.page['example'] = ui.form_card(box='1 1 4 10', items=[
             ui.text(f'file_upload={q.args.file_upload}'),
@@ -18,10 +17,6 @@ async def serve(q: Q):
             box='1 1 4 10',
             items=[
                 ui.file_upload(name='file_upload', label='Select one or more files to upload', compact=True,
-                               multiple=True, file_extensions=['jpg', 'png'], max_file_size=1, max_size=15),
-                ui.file_upload(name='file_upload2', label='Select one or more files to upload', compact=True,
-                               multiple=True, file_extensions=['jpg', 'png'], max_file_size=1, max_size=15),
-                ui.file_upload(name='file_upload3', label='Select one or more files to upload', compact=True,
                                multiple=True, file_extensions=['jpg', 'png'], max_file_size=1, max_size=15),
                 ui.button(name='submit', label='Submit', primary=True)
             ]
